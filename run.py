2dc896034597d99efeb464430118c3297331ab5f
DAIICT-CTF <setevixu@mail4gmail.com>
2018-10-04 02:43:24 +0530

    Added password checking logic

--- run.py
+++ run.py
@@ -1,2 +1,16 @@
 import welcome
 import encode_password
+import hashlib
+import getpass
+
+with open('password.txt', 'r') as f:
+	password = f.readline().split()[2]
+
+givenPassword = getpass.getpass("\nPlease enter the password to verify you are the devil: ")
+
+givenPassword = hashlib.md5(givenPassword).hexdigest()
+
+if givenPassword != password:
+	print "\nSorry, you are not the devil. Only the devil can destroy the world..."
+else:
+	print "\nThe world will be destroyed soon!!!"
