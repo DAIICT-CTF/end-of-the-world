32d646eb25564dab70e6a4c0b906ac07256d0747
DAIICT-CTF <setevixu@mail4gmail.com>
2018-10-04 02:14:50 +0530

    Added password encoding script

---
+++ encode_password.py
@@ -0,0 +1,10 @@
+import hashlib
+
+with open('password.txt', 'r') as f:
+	line = f.readline()
+	passwordState = line.split()[0]
+	password = line.split()[2]
+
+if passwordState == 'PlainPassword':
+	with open('password.txt', 'w') as f:
+		f.write("EncodedPassword : " + hashlib.md5(password).hexdigest())
